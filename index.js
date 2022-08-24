a6aed3a8f0867c61fdad6366e62853870f98b9cf
janielcarneiro <janielcl16@gmail.com>
2022-08-24 14:53:38 -0300

    cadastro de usuario feito com sucesso

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const db = require('./database.js');
 
 // pré-processamento extra para analisar o corpo das solicitações POST
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+const md5 = require("md5");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -49,6 +50,42 @@ app.get("/api/user/:id", (req, res, next) => {
         res.json({
             "message": "success",
             "data": row
+        })
+    })
+})
+
+//criando a minha rota para cadastra
+app.post("/api/user/", (req, res, next) => {
+    var errors = [];
+
+    //middleware emprovisado
+    if(!req.body.name)
+        errors.push("Digete um nome")
+    if(!req.body.password)
+        errors.push("Nenhuma senha especificada")
+    if(!req.body.email)
+        errors.push("Nenhum email especificado")
+    if(errors.length){
+        res.status(400).json({"error": errors.join(",")})
+    }
+
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: md5(req.body.password)
+    }
+
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params = [data.name, data.email, data.password]
+    db.run(sql, params, function(err, result){
+        if(err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id": this.lastID
         })
     })
 })
